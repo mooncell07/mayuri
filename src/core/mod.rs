ab5d1060d6fa096e7f23a0ac769d60ee39a6b575
Nova <mooncell07@proton.me>
2025-05-08 19:21:54 +0530

    Move WS struct to lib

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -4,4 +4,3 @@ pub mod frame;
 pub mod handshake;
 pub mod stream;
 pub mod utils;
-pub mod websocket;
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,36 @@
 pub mod core;
-pub use core::websocket::WebSocket;
+
+use core::enums::Opcode;
+use core::errors::WebSocketError;
+use core::frame::Frame;
+use core::stream::Stream;
+use core::utils::get_uri;
+
+use std::str;
+
+pub struct WebSocket {
+    pub _stream: Stream,
+}
+
+impl WebSocket {
+    pub async fn connect(uri_string: &str) -> Result<WebSocket, WebSocketError> {
+        let uri = get_uri(uri_string)?;
+        let _stream = Stream::new(&uri).await?;
+
+        Ok(Self { _stream })
+    }
+
+    pub async fn send(&mut self, data: &str) -> Result<(), WebSocketError> {
+        let mut frame = Frame::set_defaults(Opcode::Text, data.as_bytes());
+        let mut data = frame.encode()?;
+        self._stream.write(&mut data).await?;
+
+        Ok(())
+    }
+
+    pub async fn listen(&mut self) -> Result<(), WebSocketError> {
+        self._stream.read().await?;
+
+        Ok(())
+    }
+}
